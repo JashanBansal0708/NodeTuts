cea07d827ac81f994a5a770186f0986f973d16f9
JashanBansal0708 <jashanbansal7856@yahoo.com>
2018-08-22 16:13:33 +0530

    Uploaded a package on npm with name jashan, just for practice

---
+++ jashan/README.md
@@ -0,0 +1,3 @@
+# My NPM package 
+## This is just a simple test package I uploaded on the NPM for practice.
+## Enter (npm i jashan) to install this package.
--- jashan/index.js
+++ jashan/index.js
@@ -2,5 +2,5 @@
 // Run npm i jashan
 
 module.exports.jashan = function(a,b){
-    return 'Jashan Bansal, Enginner By Profession';
+    return 'Jashan Bansal, Enginner By Profession. This is updated version';
 }
